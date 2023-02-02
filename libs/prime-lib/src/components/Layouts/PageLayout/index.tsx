e15eda7f7e22f87b7f081f7955dfde9c2f872bb6
Mariia Fil <mariia.fil@extrawest.com>
2023-02-02 13:45:56 +0400

    added prime menu

--- libs/prime-lib/src/components/Layouts/PageLayout/index.tsx
+++ libs/prime-lib/src/components/Layouts/PageLayout/index.tsx
@@ -3,11 +3,11 @@ import { Menu } from "../../Menu";
 
 export const PageLayout:FC<PropsWithChildren<Record<string, unknown>>> = ({children}) => {
 	return (
-    <div className="flex w-full">
-      <div className="w-menu">
+    <div className="flex w-12">
+      <div className="w-menu m-0">
         <Menu/>
       </div>
-      <div className="w-full py-10 pl-5 pr-10">
+      <div className="w-12 py-10 pl-5 pr-10">
         {children}
       </div>
     </div>

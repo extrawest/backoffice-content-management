8bf38c3d9c90a2087b59205fc89111bed1500cc8
Mariia Fil <mariia.fil@extrawest.com>
2023-02-06 14:31:06 +0400

    added status tag component

--- libs/semantic-lib/src/components/StatusTag/index.tsx
+++ libs/semantic-lib/src/components/StatusTag/index.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
-import { StatusTagProps } from "./StatusTag.types";
+import { Label } from "semantic-ui-react";
 import { TaskTypeEnum } from "@lib/shared/types";
+import { StatusTagProps } from "./StatusTag.types";
 
 export const StatusTag: FC<StatusTagProps> = ({ type }) => {
-	const colors = `${type === TaskTypeEnum.DEFAULT ? "text-gray-800" : "text-white"}
-           ${type === TaskTypeEnum.NEW ? "bg-green-4" : ""}
-           ${type === TaskTypeEnum.URGENT ? "bg-yellow-6" : ""}
-           ${type === TaskTypeEnum.DEFAULT ? "bg-gray-400" : ""}`;
+	const color = type === TaskTypeEnum.NEW ? "green" :
+		type === TaskTypeEnum.URGENT ? "yellow" :
+			"grey";
 
 	return (
-        <span className={`uppercase text-14px ${colors} py-1 px-2 rounded-1`}>
+        <Label
+          color={color}
+        >
           {type}
-        </span>
+        </Label>
 	);
 };
